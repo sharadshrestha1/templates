8b1a7f5d9dbca1dce06651a90096144bbdc36243
sharadshrestha1 <sharadshrestha1@gmail.com>
2015-04-29 14:11:42 -0500

    angular js added

---
+++ AngJSWeb/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RouteConfig.cs" company="Microsoft">
+//   Copyright © 2015 Microsoft
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace App.AngJSWeb
+{
+    using System.Web.Routing;
+
+    using App.AngJSWeb.Routing;
+
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.Add("Default", new DefaultRoute());
+        }
+    }
+}
